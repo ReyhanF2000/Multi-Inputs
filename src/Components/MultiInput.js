b80ea456f52c08ffc0fe3971ac17982a37fca434
Reyhaneh Fakouri <r.fakouri2000@gmail.com>
2020-12-29 21:29:16 +0330

    first commit

---
+++ src/Components/MultiInput.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FaMinusCircle } from 'react-icons/fa'
+
+
+const Wrapper = styled.div`
+direction:rtl;
+display:flex;
+width:70vw;
+margin:7px auto
+`
+const Span = styled.span`
+padding:5px;
+font-size:28px;
+`
+const Input = styled.input`
+margin:5px;
+flex-grow:3;
+height:28px;
+border-radius:8px
+`
+export default function MultiInput({ value, color, onInputChange, onClickRemove }) {
+    console.log(value.length)
+    return (
+        <Wrapper>
+            {value.length !== 0 && <Span onClick={onClickRemove}>
+                <FaMinusCircle color={color} />
+            </Span>}
+            <Input
+                type='text'
+                name="value"
+                value={value}
+                onChange={onInputChange}
+            ></Input>
+        </Wrapper>
+    )
+}
---
+++ src/Components/Page.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import MultiInput from './MultiInput'
+
+const Wrapper = styled.div`
+text-align:center
+`
+const Title = styled.h1``
+
+export default function Page() {
+    const [state, setState] = useState([{ id: '0', value: '', flag: false }])
+
+    function handleInputChange(id, event) {
+        const target = event.target
+        const value = target.value
+        const copyState = [...state]
+        const index = copyState.findIndex(item => item.id === id)
+        copyState[index].value = value
+        if (copyState[index].value !== '' && !copyState[index].flag) {
+            pushItem(index, copyState)
+        }
+        else if (copyState[index].value === '' && copyState[index].flag && copyState.length !== 1) {
+            copyState.splice(index, 1)
+            setState(copyState)
+        }
+        else {
+            setState(copyState)
+        }
+    }
+
+    function pushItem(index, copyState) {
+        copyState[index].flag = true
+        const randomId = (Math.floor(Math.random() * 10000000)).toString()
+        copyState.push({ id: randomId, value: '', flag: false })
+        return setState(copyState)
+    }
+
+    function handleRemove(id) {
+        const copyState = [...state]
+        const index = copyState.findIndex(item => item.id === id)
+        if (index === copyState.length - 1 && copyState.length > 1) {
+            copyState[copyState.length - 2].flag = false
+        }
+        if (copyState.length !== 1) {
+            copyState.splice(index, 1)
+            setState(copyState)
+        }
+    }
+
+    return (<Wrapper>
+        <Title>MultyInputs...</Title>
+        {state.map(item =>
+            <MultiInput
+                key={item.id}
+                value={item.value}
+                color={state.length === 1 ? '#a2a2e44d' : 'red'}
+                onInputChange={(e) => handleInputChange(item.id, e)}
+                onClickRemove={() => handleRemove(item.id)}
+            />
+        )}
+    </Wrapper>)
+}
